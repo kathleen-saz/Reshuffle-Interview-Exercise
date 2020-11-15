5d145cbf6f5d6a8c5da681dbc0aa0bb9dcde900d
Kathleen <kathleen.saz@hotmail.com>
2020-11-15 14:31:12 +1300

    enhancements
    added gitignore
    added env file
    added connectors

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const { HttpConnector, Reshuffle } = require('reshuffle')
+const { Reshuffle, CronConnector } = require('reshuffle')
+const { NlpConnector } = require('reshuffle-nlp-connector')
+const { TwitterConnector } = require('reshuffle-twitter-connector')
 
+require('dotenv').config({path: __dirname + '/.env'})
+
+//initialise the app
 const app = new Reshuffle()
 
-const connector = new HttpConnector(app)
+//initiallise NLP connector
+const nlpConnector = new NlpConnector(app)
 
-connector.on({ method: 'GET', path: '/test' }, (event, app) => {
-  event.res.end('Hello World!')
+//initialise Cron connector
+const cronConnector = new CronConnector(app)
+
+//set event for cron connector
+cronConnector.on({ expression: '0 0 0 * * *' }, (event, app) => {
+    app.getConnector('connectors/email').send({
+      to: 'email@exmaple.com',
+      subject: 'daily report!',
+      html: 'The report itself',
+    })
+  })
+
+//configure twitter connector
+const twitter = new TwitterConnector(app, {
+  customerKey: process.env.API_KEY,
+  customerSecret: process.env.API_SECRET,
 })
 
+twitter.on({ follow: 'taylorswift13' }, async (event, app) => {
+  for (const tweet of event.tweets) {
+    const result = nlpConnector.language(tweet.text)
+    console.log('Name: ', result.name, ' Code: ', result.code)
+  }
+})
+
+//start the app
 app.start()
+
+main()
